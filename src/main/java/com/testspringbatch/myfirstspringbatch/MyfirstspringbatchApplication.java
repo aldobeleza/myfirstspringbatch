618e6be2e6aa0e4a67a995058636bd937bf79e0b
aldo.beleza <aldobeluco@gmail.com>
2021-06-12 21:47:10 -0300

    Second

---
+++ src/main/java/com/testspringbatch/myfirstspringbatch/MyfirstspringbatchApplication.java
@@ -0,0 +1,13 @@
+package com.testspringbatch.myfirstspringbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfirstspringbatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfirstspringbatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testspringbatch/myfirstspringbatch/MyfirstspringbatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testspringbatch.myfirstspringbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfirstspringbatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
